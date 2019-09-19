0ecc91d4c43fb13f219c059fe876265a60e9b10a
vincent <vincent.gbs11@gmail.com>
2019-09-19 13:47:16 -0400

    Script step 1 working

--- parsingScript.sh
+++ parsingScript.sh
@@ -12,13 +12,20 @@ then
             ### iCollege bulk download format
             ### regex only tested on mac
             studentName=$( echo $file | sed -E 's~(^[a-zA-Z]+\/\/[0-9]+-[0-9]+ - )(.+ .+)( - [a-zA-Z]{3} [0-9]{1,2}, [0-9]{4} [0-9]{1,4} [AP]M - [a-zA-Z_]+\.java)$~\2~g')
-            echo $studentName
             javaFileName=$(echo $file | sed -E 's~(^[a-zA-Z]+\/\/[0-9]+-[0-9]+ - )(.+ .+)( - [a-zA-Z]{3} [0-9]{1,2}, [0-9]{4} [0-9]{1,4} [AP]M - )([a-zA-Z_]+\.java)$~\4~g')
-            echo $javaFileName
+            valid='a-zA-Z \-' ### valid name characters
+            if [[ ! $studentName =~ [^$valid] ]]
+            then
+                studentName=$(echo $studentName | tr -d " \t\n\r") ### remove spaces
+                mkdir -p "$1/$studentName"
+                mv "$file" "$1/$studentName/$javaFileName"
+            else
+                echo "Error: $studentName is not a valid name"
+            fi
             # CLASSPATH=./
         else
             ### skip processing
-            echo "$file is not a java file"
+            echo "Error: $file is not a java file"
         fi
     done ### end forloop
 else
